771f5359159d279b1e62b980e7a60afab43b7889
omansour <spark.ouss@gmail.com>
2024-10-19 19:33:44 +0100

    PostgreSQL has been set up successfully

--- pong/settings.py
+++ pong/settings.py
@@ -89,9 +89,17 @@ WSGI_APPLICATION = 'pong.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pong_db',
+        'USER': 'omansour',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -143,7 +151,7 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'media')
 
 #Oauth
-SITE_ID = 3
+SITE_ID = 4 # Need to pay attention here
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
